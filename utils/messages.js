ab0fd8452fc23489ce72296a9186e6b9817c04a7
SaiKumarMajji <105416584+SaiKumarMajji@users.noreply.github.com>
2024-03-04 21:00:33 +0530

    Update messages.js

--- utils/messages.js
+++ utils/messages.js
@@ -4,7 +4,7 @@ function formatMessage(username, text) {
   return {
     username,
     text,
-    time: moment().tz("Asia/Kolkata").format("h:mm a"),
+      time: moment().format("h:mm a"),
   };
 }
 
